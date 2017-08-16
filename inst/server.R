c3e29c7fc5eaf25304b8bd05c2fb210562094bc7
Ildiko Czeller <czeildi@gmail.com>
2017-08-16 21:29:35 +0200

    remove file writing to some temp location

--- inst/server.R
+++ inst/server.R
@@ -4,8 +4,6 @@ shinypackage_server <- function(input, output) {
         paste(list.files(include.dirs = TRUE), collapse = "\n")
     )
 
-    file.create("shiny_temp.txt")
-
     message(readLines("global.R"))
 
     output$birth_dt <- renderDataTable(
