f921f9c24d03e556e6bfb22a5f8d4feb56a321e5
Ildiko Czeller <czeildi@gmail.com>
2017-08-16 21:07:15 +0200

    try exporting helper function

--- R/server_parts.R
+++ R/server_parts.R
@@ -1,3 +1,12 @@
+#' create filtered dt
+#'
+#' @param min_year min_year
+#' @param max_year max_year
+#'
+#' @return filtered dt
+#' @export
+#'
+#' @examples \donttest{createFilteredDt(2007, 2015)}
 createFilteredDt <- function(min_year, max_year) {
     dplyr::filter(cleaned_birth_data, year >= min_year & year <= max_year)
 }
