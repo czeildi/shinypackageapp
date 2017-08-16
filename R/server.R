70346d384eca79a6c933bea8e58b9b443666e320
Ildiko Czeller <czeildi@gmail.com>
2017-08-16 07:42:07 +0200

    copy package basics from czeildi/shinypackage

---
+++ R/server.R
@@ -0,0 +1,36 @@
+utils::globalVariables(c("year", "age", "num_birth", "education_level"))
+
+#' server function for shiny app
+#'
+#' @importFrom magrittr %>%
+#' @import ggplot2
+#' @import shiny
+#'
+#' @param input shiny input
+#' @param output shiny output
+#'
+#' @return a shiny server object
+shinypackage_server <- function(input, output) {
+    message(
+        "contents of root folder to demonstrate full unmodified access:\n",
+        paste(list.files(include.dirs = TRUE), collapse = "\n")
+    )
+
+    getFilteredBirthDt <- function() {
+        cleaned_birth_data %>%
+            dplyr::filter(year >= input$period[1] & year <= input$period[2])
+    }
+
+    output$birth_dt <- renderDataTable(
+        getFilteredBirthDt()
+    )
+
+    output$birth_summary_plot <- renderPlot({
+        getFilteredBirthDt() %>%
+            ggplot(aes(x = age, y = num_birth, fill = education_level)) +
+            geom_col(position = 'dodge') +
+            facet_grid(year ~ country) +
+            theme(legend.position = 'bottom', legend.direction = 'vertical')
+    })
+
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# shinypackage
+
+The goal of shinypackage is to test and demonstrate the possibility of packaging a shiny app with various dependencies.
+
+## Installation
+
+You can install shinypackage from github with:
+
+
+``` r
+# install.packages("devtools")
+devtools::install_github("czeildi/shinypackageapp")
+```
+## Example
+
+TODO
+
+Material: for reference, especially on the data used see [github project](https://github.com/czeildi/meetup-presentations_budapest/tree/master/Meetup_2017_06_21)
